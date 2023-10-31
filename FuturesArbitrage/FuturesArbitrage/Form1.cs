a52ea519af0cb15b6281258186ce837d3bceb4f5
Yang <uigohiiming2@gmail.com>
2023-11-01 00:34:31 +0900

    차트에 여러 줄 그리기(series추가)

--- FuturesArbitrage/FuturesArbitrage/Form1.cs
+++ FuturesArbitrage/FuturesArbitrage/Form1.cs
@@ -16,6 +16,12 @@ namespace FuturesArbitrage
 	{
 
 		double x;
+		double x2;
+		int x2idx;
+		// excel 에서 받아온 데이터 저장할 배열
+		int[] numbers = new int[1000];
+		int[] futures = new int[1000];
+		
 		public Form1()
 		{
 			InitializeComponent();
@@ -28,11 +34,23 @@ namespace FuturesArbitrage
 			timer1.Interval = 50;
 
 			chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart2.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			chart2.Series[0].Color = Color.Red;
 
-		}
+			chart2.Series.Add("Futures");
+            chart2.Series["Futures"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			chart2.Series["Futures"].Color = Color.Blue;
+            //numbers 초기화
+            for (int i = 0; i < 1000; i++)
+            {
+				numbers[i] = i + 10;
+				futures[i] = 1100 - i;
+            }
+        }
 
 		private void timer1_Tick(object sender, EventArgs e)
 		{
+			//chart1관련
 			chart1.Series[0].Points.AddXY(x, 3 * Math.Sin(5 * x) + 5 * Math.Cos(3 * x));
 
 			if (chart1.Series[0].Points.Count > 100)
@@ -42,7 +60,27 @@ namespace FuturesArbitrage
 			chart1.ChartAreas[0].AxisX.Maximum = x;
 
 			x += 0.1;
-		}
+
+			//chart2 엑셀데이터 뿌리기
+			chart2.Series[0].Points.AddXY(x2, numbers[x2idx]);
+			chart2.Series["Futures"].Points.AddXY(x2, futures[x2idx]);
+
+			if (chart2.Series[0].Points.Count > 100)
+			{
+                chart2.Series[0].Points.RemoveAt(0);
+				chart2.Series["Futures"].Points.RemoveAt(0);
+
+            }
+				
+			chart2.ChartAreas[0].AxisX.Minimum = chart2.Series[0].Points[0].XValue;
+			chart2.ChartAreas[0].AxisX.Maximum = x2;
+			chart2.ChartAreas[0].AxisY.Minimum = 0;
+			chart2.ChartAreas[0].AxisY.Maximum = 3000;
+
+            x2idx++;// 다음인덱스 가리켜야하므로
+			x2 += 0.1; //그래프 상 오른쪽에 그려야하므로
+
+        }
 
 		private void button1_Click(object sender, EventArgs e)
 		{
@@ -57,8 +95,6 @@ namespace FuturesArbitrage
 				timer1.Start();
 				button1.Text = "Stop";
 			}
-
-
 		}
 
 		public void Alert(string msg)
@@ -68,6 +104,7 @@ namespace FuturesArbitrage
 		}
 		private void button2_Click(object sender, EventArgs e)
 		{
+			
 			this.Alert("Test MEssage");
 		}
 
