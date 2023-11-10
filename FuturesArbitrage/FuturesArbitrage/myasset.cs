978b7ef6ed0a27239e7a98065097520806fbe493
Yang <uigohiiming2@gmail.com>
2023-11-10 10:55:57 +0900

    11월물이 사라졌으니, 선물 종목코드를 12월물로 변경

--- FuturesArbitrage/FuturesArbitrage/myasset.cs
+++ FuturesArbitrage/FuturesArbitrage/myasset.cs
@@ -17,18 +17,18 @@ namespace FuturesArbitrage
 {
     public partial class myasset : Form
     {
-        string access_token = "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjcyNDlkNzQ0LWQ3YzQtNDhkNy1hZjU4LTUwZTIwN2UzM2M0NiIsImlzcyI6InVub2d3IiwiZXhwIjoxNjk5NTkyMzYyLCJpYXQiOjE2OTk1MDU5NjIsImp0aSI6IlBTYnJpOVQyOThWeXhmSjAwNHg5TW5DUW54N2dLSlI4djY1OCJ9.eiomw9opIuA3sTSrfoZul9SbY7EEuIItGCgxYnawTK4bs7lXNt6VEduOXHjuFin12CpySEST3aCNE7P-Rbj9Vg";
+        string access_token = "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjVmN2NhM2FkLTg0OTUtNDA5NS1iY2I5LTU0MDdlYjI5ZjYyYSIsImlzcyI6InVub2d3IiwiZXhwIjoxNjk5NjY3NTczLCJpYXQiOjE2OTk1ODExNzMsImp0aSI6IlBTYnJpOVQyOThWeXhmSjAwNHg5TW5DUW54N2dLSlI4djY1OCJ9.Kbfy01esVVeuc2q1-OwLtOBu8OmpZmH8SmHKB_Z_Ez0q-PLeFoX6XK9x4am-xNkDWVi_FnBIqW5BaK2L-jJkow";
         //현재 선택된 북코드와 북 이름
         // Default는 CJ ENM
         private string now_book_code = "KR7035760008";
         private string now_stock_name = "CJ ENM";
         private string now_stock_code = "035760";
-        private string now_futures_code = "1DTT11000";
+        private string now_futures_code = "1DTT12000";
 
         static string[] book_code = { "KR7035760008", "KR7097950000", "KR7002380004", "KR7373220003", "KR7011070000", "KR7011780004", "KR7138040001", "KR7010140002", "KR7066970005", "KR7271560005", "KR7122870009", "KR7039030002", "KR7377300009", "KR7091700005", "KR7015760002", "KR701880005", "KR7064350002", "KR7012330007" };
         static string[] stock_name = { "CJ ENM", "CJ 제일제당", "KCC", "LG에너지솔루션", "LG이노텍", "금호석유", "메리츠금융지주", "삼성중공업", "엘앤에프", "오리온", "와이지엔터테인먼트", "이오테크닉스", "카카오페이", "파트론", "한국전력", "한온시스템", "현대로템", "현대모비스" };
         static string[] stock_code = {"035760", "097950", "002380", "373220", "011070", "011780", "138040", "010140", "066970", "271560", "122870", "039030", "377300", "091700", "015760", "018880", "064350", "012330"};
-        static string[] futures_code = {"1DTT11000", "1D1T11000", "1D3T11000", "1F5T11000", "1BYT11000", "1C3T11000", "1FKT11000", "1BFT11000", "1FMT11000", "1FJT11000", "1CQT11000", "1GCT11000", "1F7T11000", "1E3T11000", "115T11000", "1CYT11000", "1G6T11000", "120T11000" };
+        static string[] futures_code = {"1DTT12000", "1D1T12000", "1D3T12000", "1F5T12000", "1BYT12000", "1C3T12000", "1FKT12000", "1BFT12000", "1FMT12000", "1FJT12000", "1CQT12000", "1GCT12000", "1F7T12000", "1E3T12000", "115T12000", "1CYT12000", "1G6T12000", "120T12000" };
         ///////////////////////////////// 그래프 그리는 데 필요한 변수 ///////////////////////////////////////
         int k = 0;
         double r_lending = 0.04;
